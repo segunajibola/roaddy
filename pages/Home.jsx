2d121c9ecb69c0e8ccf088f5b2f96c9f1c88b3d2
Segun Ajibola <ajbl.segun@gmail.com>
2024-02-21 00:47:46 +0100

    addTailwind

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,12 +1,22 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-    return (
-        <div className="home-container">
-            <h1>You got a two-way trip, we got the vehicle.</h1>
-            <p>Relive the stress of jumping on buses. Rent the perfect car for your perfect road trip.</p>
-            <Link to="cars">Find your car</Link>
-        </div>
-    )
-};
+  return (
+    <div className="home-container relative h-[350px] bg-gradient-to-b from-black via-transparent to-transparent bg-no-repeat bg-center bg-cover text-white py-10 px-5 flex flex-col gap-y-4">
+      <h1 className="font-semibold text-2xl leading-10">
+        You got a two-way trip, we got the vehicle.
+      </h1>
+      <p className="leading-6">
+        Relive the stress of jumping on buses. Rent the perfect car for your
+        perfect road trip.
+      </p>
+      <Link
+        to="vehicles"
+        className="inline-block text-center no-underline bg-[#ff8c38] border-none w-full mt-[27px] text-white font-semibold rounded-md cursor-pointer transition-transform duration-100 ease-in-out hover:transform hover:translate-x-.5 hover:translate-y-.5"
+      >
+        Find your car
+      </Link>
+    </div>
+  );
+}
