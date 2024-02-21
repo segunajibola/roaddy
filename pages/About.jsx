e4a53db23212f2b2c61890c435035fbb883ce54e
Segun Ajibola <ajbl.segun@gmail.com>
2024-02-21 01:01:53 +0100

    addTailwind

--- pages/About.jsx
+++ pages/About.jsx
@@ -7,17 +7,17 @@ export default function About() {
     <div className="about-page-container">
       <img src={bgImg} className="max-w-full" />
       <div className="about-page-content px-6 text-[#161616] mb-14">
-        <h1 className="leading-9">
+        <h1 className="leading-6">
           Don’t squeeze in a sedan when you could relax in a van.
         </h1>
-        <p className="leading-5">
-          Our mission is to enliven your road trip with the perfect travel van
-          rental. Our vans are recertified before each trip to ensure your
-          travel plans can go off without a hitch. (Hitch costs extra 😉)
+        <p className="leading-6">
+          Our mission is to enliven your road trip with the perfect travel
+          vehicle rental. Our cars are recertified before each trip to ensure
+          your travel plans can go off without a hitch. (Hitch costs extra 😉)
         </p>
-        <p className="leading-5">
-          Our team is full of vanlife enthusiasts who know firsthand the magic
-          of touring the world on 4 wheels.
+        <p className="leading-6">
+          Our team is full of people who know firsthand the magic of touring the
+          world on 4 wheels.
         </p>
       </div>
       <div className="bg-[#ffcc8d] text-[#161616] px-8 pb-8 mx-7 rounded-md">
